116748a410589ec1a1af31c80e68cc082e608428
nilushancosta <nilushancosta@yahoo.com>
2018-11-23 12:45:46 +0530

    Set initial worker thread pool size with command line argument

--- src/main/java/com/nilushan/adaptive_concurrency_control/AdaptiveConcurrencyControl.java
+++ src/main/java/com/nilushan/adaptive_concurrency_control/AdaptiveConcurrencyControl.java
@@ -15,16 +15,17 @@ public class AdaptiveConcurrencyControl {
 	public static Logger LOGGER = LoggerFactory.getLogger(AdaptiveConcurrencyControl.class);
 
 	public static void main(String[] args) throws Exception {
-		if (args.length == 0) {
-			LOGGER.error("Argument not found! Please specify the test name as the argument");
+		if (args.length != 2) {
+			LOGGER.error("Arguments not found! Please specify the test name as the argument");
 			System.exit(-1);
 		}
 		String testName = args[0];
+		int initWorkerThreads = Integer.parseInt(args[1]);
 		ScheduledExecutorService threadPoolSizeModifier = Executors.newScheduledThreadPool(1); // Create the thread pool
 																								// to run the periodic
 																								// thread count
 																								// adjustment
-		CustomThreadPool thirdThreadPool = new CustomThreadPool(1); // Create the thread pool to handle workload
+		CustomThreadPool thirdThreadPool = new CustomThreadPool(initWorkerThreads); // Create the thread pool to handle workload
 																	// processing
 		threadPoolSizeModifier.scheduleAtFixedRate(new ThreadPoolSizeModifier(thirdThreadPool),
 				THREAD_POOL_MODIFICATION_INITIAL_DELAY, THREAD_POOL_MODIFICATION_PERIOD, TimeUnit.SECONDS);
