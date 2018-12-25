2d90f6bc88f522ca51fb6530ebd8bbdce9114bfb
nilushancosta <nilushancosta@yahoo.com>
2018-12-25 22:29:13 +0530

    Modify thread pool adjustment algorithms

---
+++ src/main/java/com/nilushan/adaptive_concurrency_control/Memory.java
@@ -0,0 +1,42 @@
+package com.nilushan.adaptive_concurrency_control;
+
+public class Memory {
+	
+	int threadPoolSize;
+	int count;
+	double value;
+	
+	public Memory(int size, int c, double val) {
+		this.threadPoolSize = size;
+		this.count = c;
+		this.value = val;
+	}
+	
+	public Memory() {
+	}
+
+	public int getThreadPoolSize() {
+		return threadPoolSize;
+	}
+
+	public void setThreadPoolSize(int threadPoolSize) {
+		this.threadPoolSize = threadPoolSize;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+
+}
